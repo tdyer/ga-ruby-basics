8f9b82a091b05145a1148dfc6dd4ed8d693305c5
Jordan Morano <jordan.morano@gmail.com>
2014-01-07 11:46:36 -0500

    added some comments and adjusted variables

--- basic_ruby.rb
+++ basic_ruby.rb
@@ -1,11 +1,16 @@
+# that is awkward - I didn't know that I had kids!
+
 my_child_age = 2
 
 puts "my child's age is #{my_child_age}"
 
-my_age = 32
+# Yes I am 31...
+
+my_age = 31
 
 puts "my age is #{my_age}"
 
+# Let's party like it is 2014??
 current_year = 2014
 
 puts "current year is #{current_year}"
@@ -18,13 +23,15 @@ puts "child's Date of Birth = #{current_year - my_child_age}"
 
 my_dob = current_year - my_age
 
+# as he gets older I get older
 my_dads_age = 57
 
 my_dads_dob = current_year - my_dads_age
 
-my_first_name = "Jen"
+#I like to stay anymous but here is my first name code
+my_first_name = "jordpo"
 
-my_last_name = "Smith"
+my_last_name = "M"
 
 my_full_name = my_first_name + " " + my_last_name
 
