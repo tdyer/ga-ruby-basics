66b38f46205d2acfa5c09fcc4fc865596b86e864
Jordan Morano <jordan.morano@gmail.com>
2014-01-07 11:42:00 -0500

    added puts

--- basic_ruby.rb
+++ basic_ruby.rb
@@ -1,10 +1,18 @@
 my_child_age = 2
 
+puts "my child's age is #{my_child_age}"
+
 my_age = 32
 
 current_year = 2014
 
+puts "current year is #{current_year}"
+
 child_dob = current_year - my_child_age
+
+puts "child's Date of Birth = #{child_dob}"
+
+puts "child's Date of Birth = #{current_year - my_child_age}"
 
 my_dob = current_year - my_age
 
@@ -17,3 +25,5 @@ my_first_name = "Jen"
 my_last_name = "Smith"
 
 my_full_name = my_first_name + " " + my_last_name
+
+puts "my full name is #{my_full_name}"
