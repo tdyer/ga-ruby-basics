77024514da695145bfef886c2fe01a9ab43d2843
Jordan Morano <jordan.morano@gmail.com>
2014-01-07 12:31:44 -0500

    add case statement

--- ruby_conditionals.rb
+++ ruby_conditionals.rb
@@ -1,7 +1,7 @@
 # learn about conditionals
 
 # if, elsif, else
-my_age = 21
+my_age = 31
 hair_color = "brown"
 
 if my_age > 21 && hair_color == "red"
@@ -16,4 +16,26 @@ puts "yep, nils equals nil" if nil == nil
 
 unless hair_color == "blue"
 	puts "Your hair is not blue but #{hair_color}."
+	#elsif ...
+end
+
+=begin
+
+here is a block comment in Ruby!!
+yeah
+
+=end
+
+my_dob = 1982
+current_year = 2014
+
+case (current_year - my_dob)
+	when 27
+		puts "What student loans??"
+	when 32
+		puts "Begin to think about the day I spend off my student loans"
+	when 97
+		puts "I can be done with my student loans"
+	else
+		puts "live in poverty"
 end
