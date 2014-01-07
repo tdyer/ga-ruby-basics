cdc9fbf3c18447c4622748901d8d8987f2d05590
Jordan Morano <jordan.morano@gmail.com>
2014-01-07 11:40:06 -0500

    created my basic_ruby file

---
+++ basic_ruby.rb
@@ -0,0 +1,19 @@
+my_child_age = 2
+
+my_age = 32
+
+current_year = 2014
+
+child_dob = current_year - my_child_age
+
+my_dob = current_year - my_age
+
+my_dads_age = 57
+
+my_dads_dob = current_year - my_dads_age
+
+my_first_name = "Jen"
+
+my_last_name = "Smith"
+
+my_full_name = my_first_name + " " + my_last_name
